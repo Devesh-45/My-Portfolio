90a0a710b626c32a0b64b269eabbfc46c6ad609a
Devesh <devesh.ashok02@gmail.com>
2025-10-15 19:16:16 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,58 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+const words = ["Developer.", "Designer.", "Programmer."]; 
+let wordIndex = 0;
+let charIndex = 0;
+const typingSpeed = 150; 
+const erasingSpeed = 100; 
+const delay = 1000; 
+const targetElement = document.getElementById("changing-text");
+
+function type() {
+    
+    const currentWord = words[wordIndex];
+
+    
+    if (charIndex < currentWord.length) {
+        targetElement.textContent += currentWord.charAt(charIndex);
+        charIndex++;
+        
+        setTimeout(type, typingSpeed);
+    } else {
+        
+        setTimeout(erase, delay);
+    }
+}
+
+function erase() {
+    const currentWord = words[wordIndex];
+
+    
+    if (charIndex > 0) {
+        targetElement.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--;
+        
+        setTimeout(erase, erasingSpeed);
+    } else {
+        
+        wordIndex++;
+       
+        if (wordIndex >= words.length) {
+            wordIndex = 0;
+        }
+        
+        setTimeout(type, typingSpeed);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    type();
+});
